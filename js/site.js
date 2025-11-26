99e9fb304bf5afc81b109a97e0016533696794b4
M-ybeme <marlomayberry.90@gmail.com>
2025-11-25 20:02:19 -0500

    liscencing update

---
+++ js/site.js
@@ -0,0 +1,15 @@
+// The DM's Toolbox – Initiative Tracker & DM Tools
+// Copyright (c) 2021–2025 Marlo. All rights reserved.
+// Build: 2025-11-25T19:00:00Z (manual)
+const DM_TOOLBOX_BUILD = {
+  appName: "The DM's Toolbox",
+  version: "1.3.0",
+  buildTimestamp: "2025-11-25T19:00:00Z",
+  author: "Marlo"
+};
+
+if (typeof console !== "undefined") {
+  console.log(
+    `${DM_TOOLBOX_BUILD.appName} v${DM_TOOLBOX_BUILD.version} – build ${DM_TOOLBOX_BUILD.buildTimestamp}`
+  );
+}
